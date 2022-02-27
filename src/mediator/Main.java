d1db3310869bb9b2b98a8aa36c1008ecf252a783
mintain <3209304627@qq.com>
2022-02-27 16:36:56 +0800

    feat(mediator): add close window event

--- src/mediator/Main.java
+++ src/mediator/Main.java
@@ -1,8 +1,5 @@
 package mediator;
 
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
 public class Main {
     public static void main(String[] args) {
         new LoginFrame("Mediator Sample");
