64d1f4b836f36bc3a5c05dcb696f66bf4d10ef32
mintain <3209304627@qq.com>
2022-02-27 15:25:51 +0800

    feat: mediator 调停者模式

---
+++ src/mediator/Main.java
@@ -0,0 +1,10 @@
+package mediator;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Main {
+    public static void main(String[] args) {
+        new LoginFrame("Mediator Sample");
+    }
+}
